91f95cccfd53e29665f7f91eefba7a70fd80174b
Harsha Vsk <harshavsk2002@gmail.com>
2022-12-01 22:29:43 +0530

    changed l9 to l6 github repo

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,6 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, where } = require("sequelize");
+const { Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -15,8 +16,10 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
-    markAsCompleted() {
-      return this.update({ completed: true });
+    setCompletionStatus() {
+      return this.update({
+        completed: !this.completed,
+      });
     }
 
     static getTodos() {
@@ -26,11 +29,54 @@ module.exports = (sequelize, DataTypes) => {
       return todos;
     }
 
+    static getOverdueItems() {
+      const overdueItems = Todo.findAll({
+        where: {
+          dueDate: { [Op.lt]: new Date() },
+          completed: { [Op.eq]: false },
+        },
+        order: [["id", "ASC"]],
+      });
+
+      return overdueItems;
+    }
+
+    static getDueTodayItems() {
+      const dueTodayItems = Todo.findAll({
+        where: {
+          dueDate: new Date(),
+          completed: { [Op.eq]: false },
+        },
+        order: [["id", "ASC"]],
+      });
+
+      return dueTodayItems;
+    }
+
+    static getDueLaterItems() {
+      const dueLaterItems = Todo.findAll({
+        where: {
+          dueDate: { [Op.gt]: new Date() },
+          completed: { [Op.eq]: false },
+        },
+        order: [["id", "ASC"]],
+      });
+
+      return dueLaterItems;
+    }
+
     deleteTodo() {
       return this.destroy({
         where: {
           id: this.id,
         },
+      });
+    }
+
+    static getCompletedTodos() {
+      return this.findAll({
+        where: { completed: { [Op.eq]: true } },
+        order: [["id", "DESC"]],
       });
     }
   }
