b5072c43ff352a071ac167adc2ebefb2897fa41e
Harsha Vsk <harshavsk2002@gmail.com>
2022-12-04 09:32:55 +0530

    updatedm

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
 app.use(
   session({
-    secret: "my-super-secret-key-2837428907583420",
+    secret: "my-super-secret-key-24576544478591464",
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
     },
